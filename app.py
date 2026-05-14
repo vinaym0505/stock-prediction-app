27d2aac4e5d6b66cbdaa4e64493381c22c476418
vinaym0505 <vinaym0505cdg@gmail.com>
2026-05-14 08:57:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return "Stock Prediction Backend Running Successfully"
+
 @app.route('/predict', methods=['POST'])
 def predict():
 
